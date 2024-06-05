51a2fcdc83e9f48f9a4907ae68e88bf322c27726
Stuti Dutta <stutidutta@aarkglobalinc.com>
2024-06-05 11:00:03 +0530

    Add project files.

---
+++ WpfApp16/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using WpfApp16.View;
+namespace WpfApp16
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNormal_Click(object sender, RoutedEventArgs e)
+        {
+            NormalWindow normalWindow = new NormalWindow();
+            normalWindow.Show();
+        }
+
+        private void btnModal_Click(object sender, RoutedEventArgs e)
+        {
+            ModalWindow modalWindow = new ModalWindow(this);
+            Opacity = 0.4;
+            modalWindow.ShowDialog();
+            Opacity = 1;
+            if(modalWindow.Success)
+            {
+                txtInput.Text = modalWindow.Input;
+            }
+        }
+    }
+}
---
+++ WpfApp16/View/NormalWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfApp16.View
+{
+    /// <summary>
+    /// Interaction logic for NormalWindow.xaml
+    /// </summary>
+    public partial class NormalWindow : Window
+    {
+        public NormalWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
